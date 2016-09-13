617d6e2f723dc0ff213a098f0f032569bed3fd83
Chelle Cruz <chellecruz@Chelles-MBP.attlocal.net>
2016-09-13 14:28:02 -0700

    Implemented function with multiple arguments

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -14,6 +14,13 @@
  ### 1. Create a function that will take in a continent and the number of countries in that continent. It should print the following sentence "____ is a continent which contains ___ countries".
  */
 // write your code here
+func continentFact(continent: String, numberOfCountries: Int) {
+    
+    print("\(continent) is a continent which contains \(numberOfCountries) countries.")
+    
+}
+
+continentFact("North America", numberOfCountries: 3)
 
 
 
@@ -25,7 +32,7 @@ func greeting(name: String, greeting: String) {
     print("\(greeting), \(name)!")
 }
 
-greeting("Danny", "Hello")
+greeting("Danny", greeting: "Hello")
 
 
 
@@ -33,8 +40,8 @@ greeting("Danny", "Hello")
 /*: question3
  ### 3. This function doesn't work, either. Can you fix the function (_not_ the call) so that it works?
  */
-func daysInMonth(month: String, days: String) {
-    print("There are \(days) in \(month)")
+func daysInMonth(month: String, days: Int) {
+    print("There are \(days) days in \(month)")
 }
 
 daysInMonth("November", days: 30)
@@ -46,6 +53,19 @@ daysInMonth("November", days: 30)
  ### 4. So far, you have created functions that take two arguments. Can you create (and call) one that takes three? Try to create a function that three parameters: a beverage, the number of bottles of that beverage, and a place you can keep those bottles. Print the sentence "____ bottles of ____ on the ____ wall."
  */
 // write your code here
+func bottles(nameOfBeverage: String, numberOfBottles: Int, placeWhereBottlesAreStored: String) {
+    
+    print("\(numberOfBottles) bottles of \(nameOfBeverage) on the \(placeWhereBottlesAreStored) wall.")
+    
+}
+
+bottles("Coke", numberOfBottles: 12, placeWhereBottlesAreStored: "refrigerator")
+
+var name = "Lemonade"
+var number = 16
+var place = "garage"
+
+bottles(name, numberOfBottles: number, placeWhereBottlesAreStored: place)
 
 
 
