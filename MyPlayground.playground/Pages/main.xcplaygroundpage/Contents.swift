3b5bd564bd59c2d77b0a9c3a8d9b6b01bc72bb3f
Taylor Bottles <taylorbottles@Taylors-Air-2.PK5001Z>
2016-09-15 11:14:19 -0700

    Problems Complete

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -13,7 +13,11 @@
 /*: question1
  ### 1. Create a function that will take in a continent and the number of countries in that continent. It should print the following sentence "____ is a continent which contains ___ countries".
  */
-// write your code here
+func continentCountries(nameOfContinent: String, numberOfCountries: Int) {
+    print("\(nameOfContinent) is a continent which contains \(numberOfCountries) countries.")
+}
+
+continentCountries(nameOfContinent: "North America", numberOfCountries: 23)
 
 
 
@@ -25,7 +29,7 @@ func greeting(name: String, greeting: String) {
     print("\(greeting), \(name)!")
 }
 
-greeting(name: "Danny", "Hello")
+greeting(name: "Danny", greeting: "Hello")
 
 
 
@@ -33,8 +37,8 @@ greeting(name: "Danny", "Hello")
 /*: question3
  ### 3. This function doesn't work, either. Can you fix the function (_not_ the call) so that it works?
  */
-func daysInMonth(month: String, days: String) {
-    print("There are \(days) in \(month)")
+func daysInMonth(month: String, days: Int) {
+    print("There are \(days) days in \(month)")
 }
 
 daysInMonth(month: "November", days: 30)
@@ -45,10 +49,11 @@ daysInMonth(month: "November", days: 30)
 /*: question4
  ### 4. So far, you have created functions that take two arguments. Can you create (and call) one that takes three? Try to create a function that three parameters: a beverage, the number of bottles of that beverage, and a place you can keep those bottles. Print the sentence "____ bottles of ____ on the ____ wall."
  */
-// write your code here
+func bottlesOnTheWall(beverage: String, numberOfBottles: Int, typeOfWall: String) {
+    print("\(numberOfBottles) bottles of \(beverage) on the \(typeOfWall) wall")
+}
 
-
-
+bottlesOnTheWall(beverage: "beer", numberOfBottles: 99, typeOfWall: "Kitchen")
 
 /*:
  Checkout the solution branch - git co solution or git checkout solution and then scroll back down to this very spot to see a link that directs you to the solutions to the above questions.
