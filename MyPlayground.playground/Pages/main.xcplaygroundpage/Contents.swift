57c4a6641853d8c738b8bc19f97cbb841fc54665
Christian Martinez <cmbp@Christians-MacBook-Pro.local>
2016-09-19 19:51:23 -0400

    Done

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -14,14 +14,16 @@
  ### 1. Create a function that will take in a continent and the number of countries in that continent. It should print the following sentence "____ is a continent which contains ___ countries".
  */
 // write your code here
-
+func firstFunction(_ nameOfContinent:String, _ numberOfCountries:Int){
+    print("\(nameOfContinent) is a continent which contains \(numberOfCountries) countries")
+}
 
 
 
 /*: question2
  ### 2. Can you see why the code below doesn't work? Fix the function call to get rid of the error.
  */
-func greeting(name: String, greeting: String) {
+func greeting(name: String, _ greeting: String) {
     print("\(greeting), \(name)!")
 }
 
@@ -33,7 +35,7 @@ greeting(name: "Danny", "Hello")
 /*: question3
  ### 3. This function doesn't work, either. Can you fix the function (_not_ the call) so that it works?
  */
-func daysInMonth(month: String, days: String) {
+func daysInMonth(month: String, days: Int) {
     print("There are \(days) in \(month)")
 }
 
@@ -46,8 +48,11 @@ daysInMonth(month: "November", days: 30)
  ### 4. So far, you have created functions that take two arguments. Can you create (and call) one that takes three? Try to create a function that three parameters: a beverage, the number of bottles of that beverage, and a place you can keep those bottles. Print the sentence "____ bottles of ____ on the ____ wall."
  */
 // write your code here
+func functionTwo(_ nameOfBeverage:String, _ numberOfBottles:Int, _ locationOfBottle:String){
+    print("\(numberOfBottles) bottles of \(nameOfBeverage) on the \(locationOfBottle) wall.")
+}
 
-
+functionTwo("Beer", 50, "Back")
 
 
 /*:
