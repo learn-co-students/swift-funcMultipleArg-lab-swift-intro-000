d59e08fd19ff7342a30b16d428005c2c7fc0653f
postsimian <postsimian@gmail.com>
2016-09-21 23:43:13 -0400

    My answers

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -14,18 +14,21 @@
  ### 1. Create a function that will take in a continent and the number of countries in that continent. It should print the following sentence "____ is a continent which contains ___ countries".
  */
 // write your code here
-
+func myLand(myContinent: String, numberOfCountries: Int) {
+    print("\(myContinent) is a continent which contains \(numberOfCountries) countries.")
+}
+myLand("America", numberOfCountries: 5)
 
 
 
 /*: question2
  ### 2. Can you see why the code below doesn't work? Fix the function call to get rid of the error.
  */
-func greeting(name: String, greeting: String) {
+func myGreeting(name: String, greeting: String) {
     print("\(greeting), \(name)!")
 }
 
-greeting(name: "Danny", "Hello")
+myGreeting("Danny", greeting: "Hello")
 
 
 
@@ -33,11 +36,11 @@ greeting(name: "Danny", "Hello")
 /*: question3
  ### 3. This function doesn't work, either. Can you fix the function (_not_ the call) so that it works?
  */
-func daysInMonth(month: String, days: String) {
+func daysInMonth(month: String, days: Int) {
     print("There are \(days) in \(month)")
 }
 
-daysInMonth(month: "November", days: 30)
+daysInMonth("November", days: 30)
 
 
 
@@ -46,8 +49,15 @@ daysInMonth(month: "November", days: 30)
  ### 4. So far, you have created functions that take two arguments. Can you create (and call) one that takes three? Try to create a function that three parameters: a beverage, the number of bottles of that beverage, and a place you can keep those bottles. Print the sentence "____ bottles of ____ on the ____ wall."
  */
 // write your code here
+func myBeverage(beverage: String, numberOfBottles: Int, bottleLocation: String) {
+    print("\(numberOfBottles) bottles of \(beverage) on the \(bottleLocation) wall.")
+}
 
+let beverage = "beer"
+let numBottles = 99
+let holder = "back"
 
+beverages(beverage, numberOfBottles: numBottles, holder: holder)
 
 
 /*:
