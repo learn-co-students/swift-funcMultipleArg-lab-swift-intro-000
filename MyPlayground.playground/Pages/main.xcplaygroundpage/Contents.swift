f3248b96450fae3928b10d1d651899ff779e0c97
Megan <Megan@macbook-pro-105.dynamic.ucsd.edu>
2016-09-23 14:58:15 -0700

    Final Solutions

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -50,11 +50,11 @@ daysInMonth(month: "November", days: "30")
  ### 4. So far, you have created functions that take two arguments. Can you create (and call) one that takes three? Try to create a function that three parameters: a beverage, the number of bottles of that beverage, and a place you can keep those bottles. Print the sentence "____ bottles of ____ on the ____ wall."
  */
 // write your code here
-func song( _ beverage: String, _ numberBottles: String, _ place: String) {
-    print("\(numberBottles) bottles of \(beverage) on the \(place) wall.")
+func song( _ beverage: String, _ numberBottles: Int, _ place: String) {
+    print("\(numberBottles) bottles of \(beverage) on the \(place).")
 }
 
-song("Soda", "99", "Old")
+song("soda", 99, "table")
 
 /*:
  Checkout the solution branch - git co solution or git checkout solution and then scroll back down to this very spot to see a link that directs you to the solutions to the above questions.
