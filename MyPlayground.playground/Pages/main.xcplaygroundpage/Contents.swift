b9e699d7d87eb69d62b6f4ff9b0e94f5ebf879b9
Megan <Megan@macbook-pro-105.dynamic.ucsd.edu>
2016-09-23 14:54:42 -0700

    Prelib Solutions

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -14,9 +14,11 @@
  ### 1. Create a function that will take in a continent and the number of countries in that continent. It should print the following sentence "____ is a continent which contains ___ countries".
  */
 // write your code here
+func countryCount(countries: String, continent: String ) {
+    print("\(continent) is a continent which contains \(countries) countries")
+}
 
-
-
+countryCount(countries: "3", continent: "North America")
 
 /*: question2
  ### 2. Can you see why the code below doesn't work? Fix the function call to get rid of the error.
@@ -25,7 +27,8 @@ func greeting(name: String, greeting: String) {
     print("\(greeting), \(name)!")
 }
 
-greeting(name: "Danny", "Hello")
+//Forgot the external variable name
+greeting(name: "Danny", greeting: "Hello")
 
 
 
@@ -37,7 +40,8 @@ func daysInMonth(month: String, days: String) {
     print("There are \(days) in \(month)")
 }
 
-daysInMonth(month: "November", days: 30)
+//days is not an Integer has to be a string
+daysInMonth(month: "November", days: "30")
 
 
 
@@ -46,9 +50,11 @@ daysInMonth(month: "November", days: 30)
  ### 4. So far, you have created functions that take two arguments. Can you create (and call) one that takes three? Try to create a function that three parameters: a beverage, the number of bottles of that beverage, and a place you can keep those bottles. Print the sentence "____ bottles of ____ on the ____ wall."
  */
 // write your code here
+func song( _ beverage: String, _ numberBottles: String, _ place: String) {
+    print("\(numberBottles) bottles of \(beverage) on the \(place) wall.")
+}
 
-
-
+song("Soda", "99", "Old")
 
 /*:
  Checkout the solution branch - git co solution or git checkout solution and then scroll back down to this very spot to see a link that directs you to the solutions to the above questions.
