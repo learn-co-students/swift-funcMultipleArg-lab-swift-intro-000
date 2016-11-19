1dbb1d6dad25596d8692e0c8770a36cab593e6d3
Asia <asia.elnemer@exa.com.sa>
2016-11-19 17:47:35 +0200

    soution

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -15,9 +15,11 @@
  */
 // write your code here
 
+func continent_Print (continent:String , countries_num:Int) {
+    print("\(continent) is a continent which contains \(countries_num) countries")
+}
 
-
-
+continent_Print(continent: "asia", countries_num: 20)
 /*: question2
  ### 2. Can you see why the code below doesn't work? Fix the function call to get rid of the error.
  */
@@ -25,7 +27,7 @@ func greeting(name: String, greeting: String) {
     print("\(greeting), \(name)!")
 }
 
-greeting(name: "Danny", "Hello")
+greeting(name: "Danny",greeting:  "Hello")
 
 
 
@@ -33,7 +35,7 @@ greeting(name: "Danny", "Hello")
 /*: question3
  ### 3. This function doesn't work, either. Can you fix the function (_not_ the call) so that it works?
  */
-func daysInMonth(month: String, days: String) {
+func daysInMonth(month: String, days: Int) {
     print("There are \(days) in \(month)")
 }
 
@@ -46,8 +48,11 @@ daysInMonth(month: "November", days: 30)
  ### 4. So far, you have created functions that take two arguments. Can you create (and call) one that takes three? Try to create a function that three parameters: a beverage, the number of bottles of that beverage, and a place you can keep those bottles. Print the sentence "____ bottles of ____ on the ____ wall."
  */
 // write your code here
+func three_arg (_ beverage :String , _ Num_bottels:Int , _ place:String) {
+    print("\(beverage) bottles of \(Num_bottels) on the \(place) wall.")
+}
 
-
+three_arg("vvvv", 20, "bbb")
 
 
 /*:
