23a253a9eda2ac8b6da8f8c345146136ffa148a1
4Ring <ukrainianslavic@gmail.com>
2016-12-20 19:05:22 -0800

    Completed Functions with multiple arguments lab

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -13,7 +13,13 @@
 /*: question1
  ### 1. Create a function that will take in a continent and the number of countries in that continent. It should print the following sentence "____ is a continent which contains ___ countries".
  */
-// write your code here
+func continentDescription(continentName: String, numberOfCountries: Int) {
+    
+    print("\(continentName) is a continent which contains \(numberOfCountries) countries.")
+    
+}
+
+continentDescription(continentName: "Antartica", numberOfCountries: 1)
 
 
 
@@ -25,7 +31,7 @@ func greeting(name: String, greeting: String) {
     print("\(greeting), \(name)!")
 }
 
-greeting(name: "Danny", "Hello")
+greeting(name: "Danny", greeting: "Hello")
 
 
 
@@ -33,8 +39,8 @@ greeting(name: "Danny", "Hello")
 /*: question3
  ### 3. This function doesn't work, either. Can you fix the function (_not_ the call) so that it works?
  */
-func daysInMonth(month: String, days: String) {
-    print("There are \(days) in \(month)")
+func daysInMonth(month: String, days: Int) {
+    print("There are \(days) days in \(month)")
 }
 
 daysInMonth(month: "November", days: 30)
@@ -45,8 +51,13 @@ daysInMonth(month: "November", days: 30)
 /*: question4
  ### 4. So far, you have created functions that take two arguments. Can you create (and call) one that takes three? Try to create a function that three parameters: a beverage, the number of bottles of that beverage, and a place you can keep those bottles. Print the sentence "____ bottles of ____ on the ____ wall."
  */
-// write your code here
+func beerSentence(numberOf bottles: Int, typeOf beverage: String, placeOf wall: String) {
+    
+    print("\(bottles) bottles of \(beverage) on the \(wall) wall)")
+    
+}
 
+beerSentence(numberOf: 99, typeOf: "beer", placeOf: "Berlin")
 
 
 
