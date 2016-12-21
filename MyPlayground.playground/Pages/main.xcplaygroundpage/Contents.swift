7d0a60d8acd2875a49b40d4efa619022e2548a7f
CG-Victor <chrisg.gonzaga@gmail.com>
2016-12-21 14:27:03 -0500

    Update Contents.swift

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -13,8 +13,19 @@
 /*: question1
  ### 1. Create a function that will take in a continent and the number of countries in that continent. It should print the following sentence "____ is a continent which contains ___ countries".
  */
-// write your code here
+func numberOfContinents(continent: String, numberOfCountries: Int) {
+    
+    continent
+    numberOfCountries
+    
+    print("\(continent) is a contient which contains \(numberOfCountries) countries")
+    
+}
 
+let continent = "Asia"
+let numberOfCountries = 50
+
+numberOfContinents(continent: continent, numberOfCountries: numberOfCountries)
 
 
 
@@ -25,7 +36,7 @@ func greeting(name: String, greeting: String) {
     print("\(greeting), \(name)!")
 }
 
-greeting(name: "Danny", "Hello")
+greeting(name: "Danny", greeting: "Hello") //basically, you must add the parallel name
 
 
 
@@ -33,7 +44,7 @@ greeting(name: "Danny", "Hello")
 /*: question3
  ### 3. This function doesn't work, either. Can you fix the function (_not_ the call) so that it works?
  */
-func daysInMonth(month: String, days: String) {
+func daysInMonth(month: String, days: Int) {
     print("There are \(days) in \(month)")
 }
 
@@ -45,7 +56,15 @@ daysInMonth(month: "November", days: 30)
 /*: question4
  ### 4. So far, you have created functions that take two arguments. Can you create (and call) one that takes three? Try to create a function that three parameters: a beverage, the number of bottles of that beverage, and a place you can keep those bottles. Print the sentence "____ bottles of ____ on the ____ wall."
  */
-// write your code here
+func beverages(beverage: String, numberOfBottles: Int, beverageKeeper: String) {
+    
+    
+        print("\(beverage) bottles of \(numberOfBottles) on the \(beverageKeeper)")
+}
+
+beverages(beverage: "Beer", numberOfBottles: 23, beverageKeeper: "cooler")
+
+
 
 
 
