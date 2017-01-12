33017873237b169cedc4049766dbe8c8646aedc7
Vinny Tran <vinnytran@Vinnys-MBP.fios-router.home>
2017-01-12 17:00:39 -0500

    Complete problems

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -13,10 +13,13 @@
 /*: question1
  ### 1. Create a function that will take in a continent and the number of countries in that continent. It should print the following sentence "____ is a continent which contains ___ countries".
  */
-// write your code here
+func NameOfContinent(continent: String, countries: Int){
+    print ("\(continent) is a continent which contains, \(countries) countries")
+}
 
-
-
+let continent = "North America"
+let countries = 23
+NameOfContinent(continent:continent, countries: countries)
 
 /*: question2
  ### 2. Can you see why the code below doesn't work? Fix the function call to get rid of the error.
@@ -25,7 +28,7 @@ func greeting(name: String, greeting: String) {
     print("\(greeting), \(name)!")
 }
 
-greeting(name: "Danny", "Hello")
+greeting(name: "Danny", greeting: "Hello")
 
 
 
@@ -33,7 +36,7 @@ greeting(name: "Danny", "Hello")
 /*: question3
  ### 3. This function doesn't work, either. Can you fix the function (_not_ the call) so that it works?
  */
-func daysInMonth(month: String, days: String) {
+func daysInMonth(month: String, days: Int) {
     print("There are \(days) in \(month)")
 }
 
@@ -45,7 +48,14 @@ daysInMonth(month: "November", days: 30)
 /*: question4
  ### 4. So far, you have created functions that take two arguments. Can you create (and call) one that takes three? Try to create a function that three parameters: a beverage, the number of bottles of that beverage, and a place you can keep those bottles. Print the sentence "____ bottles of ____ on the ____ wall."
  */
-// write your code here
+func beverages(beverage: String, numberOfBottles: Int, placeBeverage: String) {
+    print("\(numberOfBottles) bottles of \(beverage) on the \(placeBeverage) wall")
+}
+
+let beverage = "coke"
+let numberOfBottles = 8
+let placeBeverage = "cabinet"
+beverages(beverage: beverage, numberOfBottles: numberOfBottles, placeBeverage: placeBeverage)
 
 
 
