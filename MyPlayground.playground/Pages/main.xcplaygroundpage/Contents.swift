35480fc550d6cf125bae5f65344d1c2d33ecef75
SoniaTX <sonyaq@gmail.com>
2017-01-19 09:37:05 -0600

    Add completed funcMultipleArg lab

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -13,11 +13,17 @@
 /*: question1
  ### 1. Create a function that will take in a continent and the number of countries in that continent. It should print the following sentence "____ is a continent which contains ___ countries".
  */
-// write your code here
+func continentDescription(continent: String, numberOfCountries: Int) {
+    print("\(continent) is a continent which contains \(numberOfCountries) countries.")
+}
 
-
-
-
+continentDescription(continent: "North America", numberOfCountries: 23)
+continentDescription(continent: "South America", numberOfCountries: 12)
+continentDescription(continent: "Europe", numberOfCountries: 50)
+continentDescription(continent: "Asia", numberOfCountries: 48)
+continentDescription(continent: "Africa", numberOfCountries: 54)
+continentDescription(continent: "Australia", numberOfCountries: 4)
+continentDescription(continent: "Antarctica", numberOfCountries: 0)
 /*: question2
  ### 2. Can you see why the code below doesn't work? Fix the function call to get rid of the error.
  */
@@ -25,27 +31,33 @@ func greeting(name: String, greeting: String) {
     print("\(greeting), \(name)!")
 }
 
-greeting(name: "Danny", "Hello")
+greeting(name: "Danny", greeting: "Hello")
 
-
+// Yes, I can see why the code doesn't compile without the second argument so I specify the second argument ( greeting: ) for the function call.
 
 
 /*: question3
  ### 3. This function doesn't work, either. Can you fix the function (_not_ the call) so that it works?
  */
-func daysInMonth(month: String, days: String) {
+func daysInMonth(month: String, days: Int) {
     print("There are \(days) in \(month)")
 }
 
 daysInMonth(month: "November", days: 30)
 
-
+// Yes, I can fix the function in order to work by changing from String to Int for days.
 
 
 /*: question4
  ### 4. So far, you have created functions that take two arguments. Can you create (and call) one that takes three? Try to create a function that three parameters: a beverage, the number of bottles of that beverage, and a place you can keep those bottles. Print the sentence "____ bottles of ____ on the ____ wall."
  */
-// write your code here
+func threeParameters(beverage: String, numberOfBottles: Int, place: String) {
+    print("\(numberOfBottles) bottles of \(beverage) on the \(place) wall.")
+}
+
+threeParameters(beverage: "milk", numberOfBottles: 8, place: "freezer")
+threeParameters(beverage: "diet coke", numberOfBottles: 12, place: "soda display")
+threeParameters(beverage: "beer", numberOfBottles: 16, place: "alcohol display")
 
 
 
