25a53cb0b7ac8794b561952edd263b75a4ed803d
larenSakota <jedi.temple@yahoo.com>
2017-01-19 21:38:39 -0800

    Answered questions

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -13,9 +13,14 @@
 /*: question1
  ### 1. Create a function that will take in a continent and the number of countries in that continent. It should print the following sentence "____ is a continent which contains ___ countries".
  */
-// write your code here
+func continents(nameOfContinent: String, numberOfCountries: Int) {
+    print("\(nameOfContinent) is a continent which contains \(numberOfCountries) countries")
+    
+}
 
-
+var continent = "Europe"
+var countries = 45
+continents(nameOfContinent: continent, numberOfCountries: countries)
 
 
 /*: question2
@@ -25,7 +30,7 @@ func greeting(name: String, greeting: String) {
     print("\(greeting), \(name)!")
 }
 
-greeting(name: "Danny", "Hello")
+greeting(name: "Danny", greeting: "Hello")
 
 
 
@@ -33,8 +38,8 @@ greeting(name: "Danny", "Hello")
 /*: question3
  ### 3. This function doesn't work, either. Can you fix the function (_not_ the call) so that it works?
  */
-func daysInMonth(month: String, days: String) {
-    print("There are \(days) in \(month)")
+func daysInMonth(month: String, days: Int) {
+    print("There are \(days) days in \(month)")
 }
 
 daysInMonth(month: "November", days: 30)
@@ -45,7 +50,16 @@ daysInMonth(month: "November", days: 30)
 /*: question4
  ### 4. So far, you have created functions that take two arguments. Can you create (and call) one that takes three? Try to create a function that three parameters: a beverage, the number of bottles of that beverage, and a place you can keep those bottles. Print the sentence "____ bottles of ____ on the ____ wall."
  */
-// write your code here
+func beverages(typeOfBeverage: String, number: Int, location: String) {
+    print("\(number) bottles of \(typeOfBeverage) on the \(location) wall")
+}
+
+var beverage = "Mango Juice"
+var numberOfUnits = 99
+var placeInHouse = "house"
+
+beverages(typeOfBeverage: beverage, number: numberOfUnits, location: placeInHouse)
+
 
 
 
