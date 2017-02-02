c45cbda909903f7b74fb085c1243a144a70d9ba7
Tsukasa Muto <tsukasa610@gmail.com>
2017-02-03 07:23:17 +0800

    Complete problems

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -13,9 +13,14 @@
 /*: question1
  ### 1. Create a function that will take in a continent and the number of countries in that continent. It should print the following sentence "____ is a continent which contains ___ countries".
  */
-// write your code here
+func countriesInContinent (continent: String, numberOfCountries: Int) {
+    print("\(continent) is a continent which contains \(numberOfCountries) countries")
+}
 
+var continent = "Asia"
+var numberOfCountries = 42
 
+countriesInContinent(continent: continent, numberOfCountries: numberOfCountries)
 
 
 /*: question2
@@ -25,7 +30,7 @@ func greeting(name: String, greeting: String) {
     print("\(greeting), \(name)!")
 }
 
-greeting(name: "Danny", "Hello")
+greeting(name: "Danny", greeting: "Hello")
 
 
 
@@ -33,7 +38,7 @@ greeting(name: "Danny", "Hello")
 /*: question3
  ### 3. This function doesn't work, either. Can you fix the function (_not_ the call) so that it works?
  */
-func daysInMonth(month: String, days: String) {
+func daysInMonth(month: String, days: Int) {
     print("There are \(days) in \(month)")
 }
 
@@ -45,7 +50,15 @@ daysInMonth(month: "November", days: 30)
 /*: question4
  ### 4. So far, you have created functions that take two arguments. Can you create (and call) one that takes three? Try to create a function that three parameters: a beverage, the number of bottles of that beverage, and a place you can keep those bottles. Print the sentence "____ bottles of ____ on the ____ wall."
  */
-// write your code here
+func whatOnTheWhere (numberOfBottles: Int, typeOfBeverage: String, typeOfWall: String) {
+    print("\(numberOfBottles) bottles of \(typeOfBeverage) on the \(typeOfWall) wall.")
+}
+
+var numberOfBottles = 32
+var typeOfBeverage = "Beer"
+var typeOfWall = "red"
+
+whatOnTheWhere(numberOfBottles: numberOfBottles, typeOfBeverage: typeOfBeverage, typeOfWall: typeOfWall)
 
 
 
