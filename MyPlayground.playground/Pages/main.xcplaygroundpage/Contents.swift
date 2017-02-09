36345e0037f1b57d2897a971f76092f1ed834d3d
Justin E. Green <justin.emir.green@gmail.com>
2017-02-08 18:04:05 -0600

    Completed Lab

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -15,9 +15,12 @@
  */
 // write your code here
 
+func countriesInContinent(continent: String, numberOfCountries: Int) {
+    print("\(continent) is a continent which contains \(numberOfCountries) countries.")
+}
 
 
-
+countriesInContinent(continent: "Asia", numberOfCountries: 48)
 /*: question2
  ### 2. Can you see why the code below doesn't work? Fix the function call to get rid of the error.
  */
@@ -25,7 +28,7 @@ func greeting(name: String, greeting: String) {
     print("\(greeting), \(name)!")
 }
 
-greeting(name: "Danny", "Hello")
+greeting(name: "Danny", greeting: "Hello")
 
 
 
@@ -33,7 +36,7 @@ greeting(name: "Danny", "Hello")
 /*: question3
  ### 3. This function doesn't work, either. Can you fix the function (_not_ the call) so that it works?
  */
-func daysInMonth(month: String, days: String) {
+func daysInMonth(month: String, days: Int) {
     print("There are \(days) in \(month)")
 }
 
@@ -48,6 +51,9 @@ daysInMonth(month: "November", days: 30)
 // write your code here
 
 
+func awesomeDrinks(beverage: String, numberOfBottles: Int, storageLocation: String) {
+    print("\(numberOfBottles) bottles of \(beverage) on the \(storageLocation) wall.")
+}
 
-
+awesomeDrinks(beverage: "Vodka", numberOfBottles: 405, storageLocation: "blue")
 //: Click [here](https://github.com/learn-co-curriculum/swift-funcMultipleArg-lab/blob/solution/MyPlayground.playground/Pages/solution.xcplaygroundpage/Contents.swift) to see the solution.
