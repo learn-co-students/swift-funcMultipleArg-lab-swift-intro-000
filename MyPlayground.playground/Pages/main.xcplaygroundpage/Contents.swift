479280cc359f0afdfa56429dac3566a88041fc5d
Tim Stahl <timothystahl@gmail.com>
2017-02-11 15:41:04 -0700

    Add files via upload

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -13,10 +13,11 @@
 /*: question1
  ### 1. Create a function that will take in a continent and the number of countries in that continent. It should print the following sentence "____ is a continent which contains ___ countries".
  */
-// write your code here
+func continentCountries (list continent: String, numberOf countries: Int) {
+    print("\(continent) is a continent which contains \(countries) countries.")
+}
 
-
-
+continentCountries(list: "Africa", numberOf: 8)
 
 /*: question2
  ### 2. Can you see why the code below doesn't work? Fix the function call to get rid of the error.
@@ -25,7 +26,7 @@ func greeting(name: String, greeting: String) {
     print("\(greeting), \(name)!")
 }
 
-greeting(name: "Danny", "Hello")
+greeting(name: "Danny", greeting: "Hello")
 
 
 
@@ -33,8 +34,8 @@ greeting(name: "Danny", "Hello")
 /*: question3
  ### 3. This function doesn't work, either. Can you fix the function (_not_ the call) so that it works?
  */
-func daysInMonth(month: String, days: String) {
-    print("There are \(days) in \(month)")
+func daysInMonth(month: String, days: Int) {
+    print("There are \(days) days in \(month)")
 }
 
 daysInMonth(month: "November", days: 30)
@@ -45,9 +46,21 @@ daysInMonth(month: "November", days: 30)
 /*: question4
  ### 4. So far, you have created functions that take two arguments. Can you create (and call) one that takes three? Try to create a function that three parameters: a beverage, the number of bottles of that beverage, and a place you can keep those bottles. Print the sentence "____ bottles of ____ on the ____ wall."
  */
-// write your code here
+func sillySong (numberBottles: Int, favorite beverage: String, storage: String) {
+    print("\(numberBottles) bottles of \(beverage) on the \(storage) wall.")
+}
 
+var numbottles = 99
+var beverage = "huge"
+var storage = "big"
 
+sillySong(numberBottles: numbottles, favorite: beverage, storage: storage)
+
+numbottles = 37
+beverage = "Sprite"
+storage = "little"
+
+sillySong(numberBottles: numbottles, favorite: beverage, storage: storage)
 
 
 //: Click [here](https://github.com/learn-co-curriculum/swift-funcMultipleArg-lab/blob/solution/MyPlayground.playground/Pages/solution.xcplaygroundpage/Contents.swift) to see the solution.
