435e9d41092f8c40cb550d32e23fc6b3b73604cb
pazcur <pazcur@gmail.com>
2017-03-01 10:44:55 -0500

    here we go

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -13,10 +13,12 @@
 /*: question1
  ### 1. Create a function that will take in a continent and the number of countries in that continent. It should print the following sentence "____ is a continent which contains ___ countries".
  */
-// write your code here
+func countCont(continent: String, countryCount: Int) {
+    
+        print("\(continent) is a continent which contains \(countryCount) countries")
+}
 
-
-
+countCont(continent: "South America", countryCount: 10)
 
 /*: question2
  ### 2. Can you see why the code below doesn't work? Fix the function call to get rid of the error.
@@ -25,7 +27,7 @@ func greeting(name: String, greeting: String) {
     print("\(greeting), \(name)!")
 }
 
-greeting(name: "Danny", "Hello")
+greeting(name: "Danny", greeting: "Hello")
 
 
 
@@ -33,20 +35,22 @@ greeting(name: "Danny", "Hello")
 /*: question3
  ### 3. This function doesn't work, either. Can you fix the function (_not_ the call) so that it works?
  */
-func daysInMonth(month: String, days: String) {
+func daysInMonth(month: String, days: Int) {
     print("There are \(days) in \(month)")
 }
 
 daysInMonth(month: "November", days: 30)
 
-
-
-
 /*: question4
  ### 4. So far, you have created functions that take two arguments. Can you create (and call) one that takes three? Try to create a function that three parameters: a beverage, the number of bottles of that beverage, and a place you can keep those bottles. Print the sentence "____ bottles of ____ on the ____ wall."
  */
-// write your code here
+func storeBottles(of number: Int, of bevType: String, onWall type: String) {
+    
+    print("\(number) bottles of \(bevType) on the \(type) wall.")
+    
+}
 
+storeBottles(of: 30, of: "gin", onWall: "side")
 
 
 
