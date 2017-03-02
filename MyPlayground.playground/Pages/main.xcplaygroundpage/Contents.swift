953aa8ba4271060660b3fb21352f81e056661ac8
strtzy91 <strtzy91@gmail.com>
2017-03-02 06:20:03 -0800

    Complete problems

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -13,19 +13,21 @@
 /*: question1
  ### 1. Create a function that will take in a continent and the number of countries in that continent. It should print the following sentence "____ is a continent which contains ___ countries".
  */
-// write your code here
+func continentDescription(nameOfContinent: String, numberOfCountries: Int){
+    print("\(nameOfContinent) is a continent which contains \(numberOfCountries) countires")
+}
 
-
+continentDescription(nameOfContinent: "North America", numberOfCountries: 3)
 
 
 /*: question2
  ### 2. Can you see why the code below doesn't work? Fix the function call to get rid of the error.
  */
-func greeting(name: String, greeting: String) {
+func greetingSentence(name: String, greeting: String) {
     print("\(greeting), \(name)!")
 }
 
-greeting(name: "Danny", "Hello")
+greetingSentence(name: "Danny",greeting: "Hello")
 
 
 
@@ -33,7 +35,7 @@ greeting(name: "Danny", "Hello")
 /*: question3
  ### 3. This function doesn't work, either. Can you fix the function (_not_ the call) so that it works?
  */
-func daysInMonth(month: String, days: String) {
+func daysInMonth(month: String, days: Int) {
     print("There are \(days) in \(month)")
 }
 
@@ -45,8 +47,11 @@ daysInMonth(month: "November", days: 30)
 /*: question4
  ### 4. So far, you have created functions that take two arguments. Can you create (and call) one that takes three? Try to create a function that three parameters: a beverage, the number of bottles of that beverage, and a place you can keep those bottles. Print the sentence "____ bottles of ____ on the ____ wall."
  */
-// write your code here
+func beerSong(numberOfBottles: Int,typeOfBeverage: String,locationOfBottles: String){
+    print("\(numberOfBottles) bottles of \(typeOfBeverage) on the \(locationOfBottles)!")
+}
 
+beerSong(numberOfBottles: 99, typeOfBeverage: "beer", locationOfBottles: "wall")
 
 
 
