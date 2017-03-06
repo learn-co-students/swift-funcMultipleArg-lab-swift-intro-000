212aa06209e049c0c47311addb396c14f8835845
Phayton7 <idreamit26@gmail.com>
2017-03-06 23:14:06 +0100

    Finished this lab too

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -13,9 +13,11 @@
 /*: question1
  ### 1. Create a function that will take in a continent and the number of countries in that continent. It should print the following sentence "____ is a continent which contains ___ countries".
  */
-// write your code here
+func numberOfCountries(continent: String, countries: Int){
+    print("\(continent) is a continent which contains \(countries) countries")
+}
 
-
+numberOfCountries(continent: "Europe", countries: 44)
 
 
 /*: question2
@@ -25,7 +27,7 @@ func greeting(name: String, greeting: String) {
     print("\(greeting), \(name)!")
 }
 
-greeting(name: "Danny", "Hello")
+greeting(name: "Danny", greeting: "Hello")
 
 
 
@@ -37,7 +39,7 @@ func daysInMonth(month: String, days: String) {
     print("There are \(days) in \(month)")
 }
 
-daysInMonth(month: "November", days: 30)
+daysInMonth(month: "November", days: "thirteen days")
 
 
 
@@ -45,9 +47,14 @@ daysInMonth(month: "November", days: 30)
 /*: question4
  ### 4. So far, you have created functions that take two arguments. Can you create (and call) one that takes three? Try to create a function that three parameters: a beverage, the number of bottles of that beverage, and a place you can keep those bottles. Print the sentence "____ bottles of ____ on the ____ wall."
  */
-// write your code here
+func beverages(beverage: String, numberOfBottles: Int, holder: String) {
+    print("\(numberOfBottles) bottles of \(beverage) on the \(holder) wall")
+}
 
+let beverage = "soda"
+let numBottles = 64
+let holder = "counter top"
 
-
+beverages(beverage: beverage, numberOfBottles: numBottles, holder: holder)
 
 //: Click [here](https://github.com/learn-co-curriculum/swift-funcMultipleArg-lab/blob/solution/MyPlayground.playground/Pages/solution.xcplaygroundpage/Contents.swift) to see the solution.
