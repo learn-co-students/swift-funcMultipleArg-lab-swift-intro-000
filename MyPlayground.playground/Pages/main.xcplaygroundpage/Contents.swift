636c4f3e955c76e44c26e93c5d8b8860e1dd29fd
Marc Lacerna <marcyvan.lacenra@gmail.com>
2017-03-15 21:33:12 -0400

    commit number 1

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -14,6 +14,13 @@
  ### 1. Create a function that will take in a continent and the number of countries in that continent. It should print the following sentence "____ is a continent which contains ___ countries".
  */
 // write your code here
+func Count(continent: String, countries: Int)
+{
+    print("\(continent) is a continent which contains \(countries) countries")
+}
+
+Count(continent: "North America", countries: 23)
+
 
 
 
@@ -25,7 +32,7 @@ func greeting(name: String, greeting: String) {
     print("\(greeting), \(name)!")
 }
 
-greeting(name: "Danny", "Hello")
+greeting(name: "Danny", greeting: "Hello")
 
 
 
@@ -33,7 +40,7 @@ greeting(name: "Danny", "Hello")
 /*: question3
  ### 3. This function doesn't work, either. Can you fix the function (_not_ the call) so that it works?
  */
-func daysInMonth(month: String, days: String) {
+func daysInMonth(month: String, days: Int) {
     print("There are \(days) in \(month)")
 }
 
@@ -46,6 +53,12 @@ daysInMonth(month: "November", days: 30)
  ### 4. So far, you have created functions that take two arguments. Can you create (and call) one that takes three? Try to create a function that three parameters: a beverage, the number of bottles of that beverage, and a place you can keep those bottles. Print the sentence "____ bottles of ____ on the ____ wall."
  */
 // write your code here
+func threeParam(beverage: String, noBeverage: Int, color: String)
+{
+    print("\(noBeverage) bottles of \(beverage) on the \(color) wall.")
+}
+
+threeParam(beverage: "beer", noBeverage: 10, color: "blue")
 
 
 
