6104d3959b899fcd9f8255899b236356d2e64500
Maneesh <maneesh.yadav@ahold.com>
2017-04-09 13:20:41 +0200

    Multiple Argument Lab

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -14,10 +14,12 @@
  ### 1. Create a function that will take in a continent and the number of countries in that continent. It should print the following sentence "____ is a continent which contains ___ countries".
  */
 // write your code here
+func numberOfCountries(continent: String, nuberOfCountry: Int)
+{
+    print("\(continent) is a continent which contains \(nuberOfCountry) countries")
+}
 
-
-
-
+numberOfCountries(continent: "Antartica", nuberOfCountry: 0)
 /*: question2
  ### 2. Can you see why the code below doesn't work? Fix the function call to get rid of the error.
  */
@@ -25,7 +27,7 @@ func greeting(name: String, greeting: String) {
     print("\(greeting), \(name)!")
 }
 
-greeting(name: "Danny", "Hello")
+greeting(name: "Danny", greeting: "Hello")
 
 
 
@@ -37,7 +39,7 @@ func daysInMonth(month: String, days: String) {
     print("There are \(days) in \(month)")
 }
 
-daysInMonth(month: "November", days: 30)
+daysInMonth(month: "November", days: "30")
 
 
 
@@ -47,7 +49,10 @@ daysInMonth(month: "November", days: 30)
  */
 // write your code here
 
+func ThreeArgFuntion(bev: String, bottles: Int, place: String) {
+    print("\(bev) bottles of \(bottles) on the \(place) wall.")
+}
 
-
+ThreeArgFuntion(bev: "Coke", bottles: 5, place: "side")
 
 //: Click [here](https://github.com/learn-co-curriculum/swift-funcMultipleArg-lab/blob/solution/MyPlayground.playground/Pages/solution.xcplaygroundpage/Contents.swift) to see the solution.
