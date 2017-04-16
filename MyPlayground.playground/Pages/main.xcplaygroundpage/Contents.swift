f5ff24b72d5dd9f992e53eb40b53bf5c48808700
Fr3qu3ntFly3r <bszamody@gmail.com>
2017-04-16 12:44:25 +0200

    Solution

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -13,7 +13,18 @@
 /*: question1
  ### 1. Create a function that will take in a continent and the number of countries in that continent. It should print the following sentence "____ is a continent which contains ___ countries".
  */
-// write your code here
+func continentDescription(continent cont: String, countries countryCount: Int) {
+    if countryCount == 1 {
+    print("\(cont) is a continent which is also a country")
+    } else {
+        print("\(cont) is a continent which contains \(countryCount) countries")
+    }
+    
+}
+
+continentDescription(continent: "Australia", countries: 1)
+continentDescription(continent: "Europe", countries: 32)
+
 
 
 
@@ -25,7 +36,7 @@ func greeting(name: String, greeting: String) {
     print("\(greeting), \(name)!")
 }
 
-greeting(name: "Danny", "Hello")
+greeting(name: "Danny",greeting: "Hello")
 
 
 
@@ -33,7 +44,7 @@ greeting(name: "Danny", "Hello")
 /*: question3
  ### 3. This function doesn't work, either. Can you fix the function (_not_ the call) so that it works?
  */
-func daysInMonth(month: String, days: String) {
+func daysInMonth(month: String, days: Int) {
     print("There are \(days) in \(month)")
 }
 
@@ -45,9 +56,11 @@ daysInMonth(month: "November", days: 30)
 /*: question4
  ### 4. So far, you have created functions that take two arguments. Can you create (and call) one that takes three? Try to create a function that three parameters: a beverage, the number of bottles of that beverage, and a place you can keep those bottles. Print the sentence "____ bottles of ____ on the ____ wall."
  */
-// write your code here
+func drinks (drink: String, quantity: Int, location: String) {
+    print("\(quantity) bottles of \(drink) on the \(location) wall.")
+}
 
-
+drinks(drink: "wine", quantity: 12, location: "wine rack")
 
 
 //: Click [here](https://github.com/learn-co-curriculum/swift-funcMultipleArg-lab/blob/solution/MyPlayground.playground/Pages/solution.xcplaygroundpage/Contents.swift) to see the solution.
