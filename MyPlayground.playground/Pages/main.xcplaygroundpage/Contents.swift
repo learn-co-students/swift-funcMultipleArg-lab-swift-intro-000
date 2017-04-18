c2433fa9915fc1f7f1c89ad2786eb60fcfdf749a
vincentliu130 <she4869@hotmail.com>
2017-04-18 13:10:47 -0400

    finished this lab

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -14,8 +14,11 @@
  ### 1. Create a function that will take in a continent and the number of countries in that continent. It should print the following sentence "____ is a continent which contains ___ countries".
  */
 // write your code here
+func numbers(of countries: Int, within continent: String) {
+    print("\(continent) is a continent which contains \(countries) countries.")
+}
 
-
+numbers(of: 10, within: "America")
 
 
 /*: question2
@@ -25,7 +28,7 @@ func greeting(name: String, greeting: String) {
     print("\(greeting), \(name)!")
 }
 
-greeting(name: "Danny", "Hello")
+greeting(name: "Danny", greeting: "Hello")
 
 
 
@@ -37,7 +40,7 @@ func daysInMonth(month: String, days: String) {
     print("There are \(days) in \(month)")
 }
 
-daysInMonth(month: "November", days: 30)
+daysInMonth(month: "November", days: "thirty")
 
 
 
@@ -46,8 +49,10 @@ daysInMonth(month: "November", days: 30)
  ### 4. So far, you have created functions that take two arguments. Can you create (and call) one that takes three? Try to create a function that three parameters: a beverage, the number of bottles of that beverage, and a place you can keep those bottles. Print the sentence "____ bottles of ____ on the ____ wall."
  */
 // write your code here
+func beverageDetail(of beverage: String, withNumber number: Int, inPlace place: String) {
+    print("\(number) bottles of \(beverage) on the \(place) wall.")
+}
 
-
-
+beverageDetail(of: "Coke", withNumber: 10, inPlace: "Fridge")
 
 //: Click [here](https://github.com/learn-co-curriculum/swift-funcMultipleArg-lab/blob/solution/MyPlayground.playground/Pages/solution.xcplaygroundpage/Contents.swift) to see the solution.
