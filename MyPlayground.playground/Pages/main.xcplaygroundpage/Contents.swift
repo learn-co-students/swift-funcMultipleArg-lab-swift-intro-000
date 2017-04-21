a41d82e8bcee5e1b345de74c31e1974dc41a2fbd
ChicagoSwiftDeveloper <abdullah.jawaad@gmail.com>
2017-04-21 10:22:28 -0500

    finished

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -25,7 +25,7 @@ func greeting(name: String, greeting: String) {
     print("\(greeting), \(name)!")
 }
 
-greeting(name: "Danny", "Hello")
+greeting(name: "Danny", greeting: "Hello")
 
 
 
@@ -33,7 +33,7 @@ greeting(name: "Danny", "Hello")
 /*: question3
  ### 3. This function doesn't work, either. Can you fix the function (_not_ the call) so that it works?
  */
-func daysInMonth(month: String, days: String) {
+func daysInMonth(month: String, days: Int) {
     print("There are \(days) in \(month)")
 }
 
@@ -47,6 +47,11 @@ daysInMonth(month: "November", days: 30)
  */
 // write your code here
 
+func questionFour(beverage: String, numberOfBottles: Int, place: String) {
+    print("\(numberOfBottles) bottles of \(beverage) on the \(place) wall.")
+}
+
+questionFour(beverage: "Orange Juice", numberOfBottles: 93, place: "brick")
 
 
 
