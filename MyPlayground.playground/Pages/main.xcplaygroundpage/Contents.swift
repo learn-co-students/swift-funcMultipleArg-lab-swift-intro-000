a6caff520a66e0ebfd6ac52a04720328de21ddf2
tkueny <tkueny@fordham.edu>
2017-05-01 20:39:47 -0400

    Multiple argument functions submission

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -14,18 +14,21 @@
  ### 1. Create a function that will take in a continent and the number of countries in that continent. It should print the following sentence "____ is a continent which contains ___ countries".
  */
 // write your code here
-
+func continentsAndCountries(continent: String, countries: Int)
+{
+    print("\(continent) is a continent which contains \(countries) countries.")
+}
 
 
 
 /*: question2
  ### 2. Can you see why the code below doesn't work? Fix the function call to get rid of the error.
  */
-func greeting(name: String, greeting: String) {
-    print("\(greeting), \(name)!")
+func greeting(name: String, greetingWord: String) {
+    print("\(greetingWord), \(name)!")
 }
 
-greeting(name: "Danny", "Hello")
+greeting(name: "Danny", greetingWord: "Hello")
 
 
 
@@ -33,7 +36,7 @@ greeting(name: "Danny", "Hello")
 /*: question3
  ### 3. This function doesn't work, either. Can you fix the function (_not_ the call) so that it works?
  */
-func daysInMonth(month: String, days: String) {
+func daysInMonth(month: String, days: Int) {
     print("There are \(days) in \(month)")
 }
 
@@ -46,7 +49,11 @@ daysInMonth(month: "November", days: 30)
  ### 4. So far, you have created functions that take two arguments. Can you create (and call) one that takes three? Try to create a function that three parameters: a beverage, the number of bottles of that beverage, and a place you can keep those bottles. Print the sentence "____ bottles of ____ on the ____ wall."
  */
 // write your code here
+func singMeASong(number: Int, drink: String, adjective: String){
+    print("\(number) bottles of \(drink) on the \(adjective) wall.")
+}
 
+singMeASong(number: 99, drink: "beer", adjective: "")
 
 
 
