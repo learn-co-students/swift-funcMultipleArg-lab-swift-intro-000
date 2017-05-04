d4fa2e75bfa37bb07ff7b979f0b7703af5ba5017
marisajanae <marisa.briggs.nyc@gmail.com>
2017-05-04 15:53:05 -0400

    Complete problems

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -13,8 +13,12 @@
 /*: question1
  ### 1. Create a function that will take in a continent and the number of countries in that continent. It should print the following sentence "____ is a continent which contains ___ countries".
  */
-// write your code here
-
+func numberOfCountriesInAContinent(){
+    let continent = String()
+    let numberOfCountries = Int()
+    print("\(continent) is a continent which contains \(numberOfCountries) countries.")
+}
+numberOfCountriesInAContinent()
 
 
 
@@ -24,8 +28,7 @@
 func greeting(name: String, greeting: String) {
     print("\(greeting), \(name)!")
 }
-
-greeting(name: "Danny", "Hello")
+greeting(name: "Danny", greeting: "Hello")
 
 
 
@@ -36,8 +39,7 @@ greeting(name: "Danny", "Hello")
 func daysInMonth(month: String, days: String) {
     print("There are \(days) in \(month)")
 }
-
-daysInMonth(month: "November", days: 30)
+daysInMonth(month: "November", days: "30")
 
 
 
@@ -45,9 +47,10 @@ daysInMonth(month: "November", days: 30)
 /*: question4
  ### 4. So far, you have created functions that take two arguments. Can you create (and call) one that takes three? Try to create a function that three parameters: a beverage, the number of bottles of that beverage, and a place you can keep those bottles. Print the sentence "____ bottles of ____ on the ____ wall."
  */
-// write your code here
-
-
+func numberOfBottlesOnTheWall(beverage: String, numberOfBottles: Int, placeForBottles: String){
+    print("\(numberOfBottles) bottles of \(beverage) on the \(placeForBottles) wall.")
+}
+numberOfBottlesOnTheWall(beverage: "Beer", numberOfBottles: 21, placeForBottles: "Irish")
 
 
 //: Click [here](https://github.com/learn-co-curriculum/swift-funcMultipleArg-lab/blob/solution/MyPlayground.playground/Pages/solution.xcplaygroundpage/Contents.swift) to see the solution.
