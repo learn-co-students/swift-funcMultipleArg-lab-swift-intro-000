395d4376da2f92ca15a9d885c0a07811bbda51c6
sbkbrah <k.stannard@live.com>
2017-05-15 15:46:13 -0500

    Complete problems

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -16,8 +16,11 @@
 // write your code here
 
 
+func contDetails(continent: String, numCountries: Int) {
+    print("\(continent) is a continent which contains \(numCountries) countries")
+}
 
-
+contDetails(continent: "Merica", numCountries: 1)
 /*: question2
  ### 2. Can you see why the code below doesn't work? Fix the function call to get rid of the error.
  */
@@ -25,7 +28,7 @@ func greeting(name: String, greeting: String) {
     print("\(greeting), \(name)!")
 }
 
-greeting(name: "Danny", "Hello")
+greeting(name: "Danny", greeting: "Hello")
 
 
 
@@ -37,7 +40,7 @@ func daysInMonth(month: String, days: String) {
     print("There are \(days) in \(month)")
 }
 
-daysInMonth(month: "November", days: 30)
+daysInMonth(month: "November", days: "30")
 
 
 
@@ -48,6 +51,10 @@ daysInMonth(month: "November", days: 30)
 // write your code here
 
 
+func soda(beverage: String, bottles: Int, place: String) {
+    print("\(bottles) bottles of \(beverage) on the \(place) wall.")
+}
 
+soda(beverage: "coke", bottles: 30, place: "fat gay")
 
 //: Click [here](https://github.com/learn-co-curriculum/swift-funcMultipleArg-lab/blob/solution/MyPlayground.playground/Pages/solution.xcplaygroundpage/Contents.swift) to see the solution.
