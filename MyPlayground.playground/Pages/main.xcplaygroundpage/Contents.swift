9d726e013412aebb82336ee40f91413ac148a897
Gronemann <htcfiber@gmail.com>
2017-07-30 00:51:07 +0200

    My answers

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -13,9 +13,11 @@
 /*: question1
  ### 1. Create a function that will take in a continent and the number of countries in that continent. It should print the following sentence "____ is a continent which contains ___ countries".
  */
-// write your code here
-
-
+func continetFacts(continent: String, countriesAmont: Int) {
+    print("\(continent) is a continent which contains \(countriesAmont) contries.")
+    
+}
+continetFacts(continent: "Europe", countriesAmont: 10 )
 
 
 /*: question2
@@ -25,7 +27,7 @@ func greeting(name: String, greeting: String) {
     print("\(greeting), \(name)!")
 }
 
-greeting(name: "Danny", "Hello")
+greeting(name: "Danny", greeting: "Hello")
 
 
 
@@ -33,8 +35,8 @@ greeting(name: "Danny", "Hello")
 /*: question3
  ### 3. This function doesn't work, either. Can you fix the function (_not_ the call) so that it works?
  */
-func daysInMonth(month: String, days: String) {
-    print("There are \(days) in \(month)")
+func daysInMonth(month: String, days: Int) {
+    print("There are \(days) days in \(month)")
 }
 
 daysInMonth(month: "November", days: 30)
@@ -45,9 +47,16 @@ daysInMonth(month: "November", days: 30)
 /*: question4
  ### 4. So far, you have created functions that take two arguments. Can you create (and call) one that takes three? Try to create a function that three parameters: a beverage, the number of bottles of that beverage, and a place you can keep those bottles. Print the sentence "____ bottles of ____ on the ____ wall."
  */
-// write your code here
+func bottleNeck(name: String, amount: Int, place: String) {
+    
+    print("\(amount) bottles of \(name) on the \(place) wall.")
+}
+// bottleNeck(name: "Rum", amount: 13, place: "northern")
 
+let name = "Pepsi"
+let amount = 17
+let place = "upper"
 
-
+bottleNeck(name: name, amount: amount, place: place)
 
 //: Click [here](https://github.com/learn-co-curriculum/swift-funcMultipleArg-lab/blob/solution/MyPlayground.playground/Pages/solution.xcplaygroundpage/Contents.swift) to see the solution.
