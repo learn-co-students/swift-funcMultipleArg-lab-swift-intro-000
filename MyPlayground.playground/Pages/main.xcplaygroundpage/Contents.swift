b871e1a9feabb5084347d4f76bc8d92b3f200464
Mohammed Abdulkereem <HorizonDragon@outlook.com>
2017-08-01 23:26:33 +0300

    Finished Excercies

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -16,8 +16,11 @@
 // write your code here
 
 
+func continentsAndCountries(continent: String, countries: Int) -> String {
+    return "\(continent) is a continent which contains \(countries) countries";
+}
 
-
+continentsAndCountries(continent: "Australia", countries: 1);
 /*: question2
  ### 2. Can you see why the code below doesn't work? Fix the function call to get rid of the error.
  */
@@ -25,7 +28,7 @@ func greeting(name: String, greeting: String) {
     print("\(greeting), \(name)!")
 }
 
-greeting(name: "Danny", "Hello")
+greeting(name: "Danny", greeting:  "Hello")
 
 
 
@@ -33,7 +36,7 @@ greeting(name: "Danny", "Hello")
 /*: question3
  ### 3. This function doesn't work, either. Can you fix the function (_not_ the call) so that it works?
  */
-func daysInMonth(month: String, days: String) {
+func daysInMonth(month: String, days: Int) {
     print("There are \(days) in \(month)")
 }
 
@@ -47,7 +50,11 @@ daysInMonth(month: "November", days: 30)
  */
 // write your code here
 
+func myFunc(beverage: String, numberOfBottles: Int, place: String) {
+    print("\(numberOfBottles) bottles of \(beverage) on the \(place) wall.");
+}
 
+myFunc(beverage: "Milk", numberOfBottles: 3, place: "Under");
 
 
 //: Click [here](https://github.com/learn-co-curriculum/swift-funcMultipleArg-lab/blob/solution/MyPlayground.playground/Pages/solution.xcplaygroundpage/Contents.swift) to see the solution.
