98eede3a7d765777f2a1666c0ff72de655745f09
mffarrow <mffarrow@gmail.com>
2017-08-23 12:28:40 -0700

    Complete problems

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -13,11 +13,11 @@
 /*: question1
  ### 1. Create a function that will take in a continent and the number of countries in that continent. It should print the following sentence "____ is a continent which contains ___ countries".
  */
-// write your code here
+func howManyCountries(continentName: String, countryNumber: Int) {
+    print("\(continentName) is a continent which contains \(countryNumber) countries.")
+}
 
-
-
-
+howManyCountries(continentName: "Africa", countryNumber:54)
 /*: question2
  ### 2. Can you see why the code below doesn't work? Fix the function call to get rid of the error.
  */
@@ -25,7 +25,7 @@ func greeting(name: String, greeting: String) {
     print("\(greeting), \(name)!")
 }
 
-greeting(name: "Danny", "Hello")
+greeting(name: "Danny", greeting: "Hello")
 
 
 
@@ -33,8 +33,8 @@ greeting(name: "Danny", "Hello")
 /*: question3
  ### 3. This function doesn't work, either. Can you fix the function (_not_ the call) so that it works?
  */
-func daysInMonth(month: String, days: String) {
-    print("There are \(days) in \(month)")
+func daysInMonth(month: String, days: Int) {
+    print("There are \(days) days in \(month).")
 }
 
 daysInMonth(month: "November", days: 30)
@@ -45,9 +45,9 @@ daysInMonth(month: "November", days: 30)
 /*: question4
  ### 4. So far, you have created functions that take two arguments. Can you create (and call) one that takes three? Try to create a function that three parameters: a beverage, the number of bottles of that beverage, and a place you can keep those bottles. Print the sentence "____ bottles of ____ on the ____ wall."
  */
-// write your code here
+func howManyBottles(beverage: String, bottleNumber: Int, place: String) {
+    print("\(bottleNumber) bottles of \(beverage) on the \(place) wall.")
+}
 
-
-
-
+howManyBottles(beverage: "soda", bottleNumber: 99, place: "west")
 //: Click [here](https://github.com/learn-co-curriculum/swift-funcMultipleArg-lab/blob/solution/MyPlayground.playground/Pages/solution.xcplaygroundpage/Contents.swift) to see the solution.
