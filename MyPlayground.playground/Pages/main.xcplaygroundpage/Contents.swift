c2da37f3bb6547e9ef28455cfbd4de9c328619a3
soapia <sofia.ongele@ealas.org>
2017-09-20 14:48:59 -0700

    not afraid of COMMITment

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -15,8 +15,11 @@
  */
 // write your code here
 
+func places(continent: String, countries: Int) {
+    print("\(continent) is a continent which contains \(countries) countries")
+}
 
-
+places(continent: "Antarctica", countries: 1)
 
 /*: question2
  ### 2. Can you see why the code below doesn't work? Fix the function call to get rid of the error.
@@ -25,29 +28,26 @@ func greeting(name: String, greeting: String) {
     print("\(greeting), \(name)!")
 }
 
-greeting(name: "Danny", "Hello")
-
-
-
+greeting(name: "Danny", greeting: "Hello")
 
 /*: question3
  ### 3. This function doesn't work, either. Can you fix the function (_not_ the call) so that it works?
  */
-func daysInMonth(month: String, days: String) {
+func daysInMonth(month: String, days: Int) {
     print("There are \(days) in \(month)")
 }
 
 daysInMonth(month: "November", days: 30)
-
-
-
 
 /*: question4
  ### 4. So far, you have created functions that take two arguments. Can you create (and call) one that takes three? Try to create a function that three parameters: a beverage, the number of bottles of that beverage, and a place you can keep those bottles. Print the sentence "____ bottles of ____ on the ____ wall."
  */
 // write your code here
 
+func drunk(number: Int, beverage: String, location: String) {
+    print("\(number) bottles of \(beverage) on the \(location)")
+}
 
-
+drunk(number: 99, beverage: "beer", location: "wall")
 
 //: Click [here](https://github.com/learn-co-curriculum/swift-funcMultipleArg-lab/blob/solution/MyPlayground.playground/Pages/solution.xcplaygroundpage/Contents.swift) to see the solution.
