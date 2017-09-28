1dfbb45896f57c6e77aba851c7fbee977f81bb16
mmb16 <mmb583@nyu.edu>
2017-09-28 12:09:50 -0400

    Lab complete

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -13,9 +13,14 @@
 /*: question1
  ### 1. Create a function that will take in a continent and the number of countries in that continent. It should print the following sentence "____ is a continent which contains ___ countries".
  */
-// write your code here
+func continentAndCountries(continent: String,countries: Int) {
+    print("\(continent) is a continent which contains \(countries)")
+}
 
-
+let continent = "Africa"
+let countries = 54
+//functions accepts a continent and the number of countries within and print a sentence
+continentAndCountries(continent: continent, countries: countries)
 
 
 /*: question2
@@ -25,7 +30,7 @@ func greeting(name: String, greeting: String) {
     print("\(greeting), \(name)!")
 }
 
-greeting(name: "Danny", "Hello")
+greeting(name: "Danny", greeting:  "Hello")
 
 
 
@@ -33,7 +38,7 @@ greeting(name: "Danny", "Hello")
 /*: question3
  ### 3. This function doesn't work, either. Can you fix the function (_not_ the call) so that it works?
  */
-func daysInMonth(month: String, days: String) {
+func daysInMonth(month: String, days: Int) {
     print("There are \(days) in \(month)")
 }
 
@@ -45,7 +50,15 @@ daysInMonth(month: "November", days: 30)
 /*: question4
  ### 4. So far, you have created functions that take two arguments. Can you create (and call) one that takes three? Try to create a function that three parameters: a beverage, the number of bottles of that beverage, and a place you can keep those bottles. Print the sentence "____ bottles of ____ on the ____ wall."
  */
-// write your code here
+func bottlesOnTheWall (beverage: String, bottles: Int, wall: String) {
+    print("\(bottles) bottles of \(beverage) on the \(wall) wall.")
+
+}
+let beverage = "Kombucha"
+let bottles = 5
+let wall = "shelf"
+
+bottlesOnTheWall(beverage: beverage, bottles: bottles, wall: wall)
 
 
 
