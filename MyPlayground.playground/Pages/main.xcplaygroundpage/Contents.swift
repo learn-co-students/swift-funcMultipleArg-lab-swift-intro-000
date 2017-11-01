c36254a35fb08c1bd59592f93908ce3c0bc50eed
softwarefarmer <suthar@hotmail.com>
2017-11-01 16:55:45 -0400

    lab completed

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -13,10 +13,13 @@
 /*: question1
  ### 1. Create a function that will take in a continent and the number of countries in that continent. It should print the following sentence "____ is a continent which contains ___ countries".
  */
-// write your code here
+func numberOfCountries(continent: String, numberOfCountry: Int){
+    print("\(continent) is a continent which contains \(numberOfCountry) countries")
+}
 
+numberOfCountries(continent: "North America", numberOfCountry: 23)
 
-
+numberOfCountries(continent: "Africa", numberOfCountry: 54)
 
 /*: question2
  ### 2. Can you see why the code below doesn't work? Fix the function call to get rid of the error.
@@ -25,15 +28,14 @@ func greeting(name: String, greeting: String) {
     print("\(greeting), \(name)!")
 }
 
-greeting(name: "Danny", "Hello")
-
+greeting(name: "Danny", greeting: "Hello") //the argument name must be passed
 
 
 
 /*: question3
  ### 3. This function doesn't work, either. Can you fix the function (_not_ the call) so that it works?
  */
-func daysInMonth(month: String, days: String) {
+func daysInMonth(month: String, days: Int) { // days must be Int
     print("There are \(days) in \(month)")
 }
 
@@ -41,13 +43,13 @@ daysInMonth(month: "November", days: 30)
 
 
 
-
 /*: question4
  ### 4. So far, you have created functions that take two arguments. Can you create (and call) one that takes three? Try to create a function that three parameters: a beverage, the number of bottles of that beverage, and a place you can keep those bottles. Print the sentence "____ bottles of ____ on the ____ wall."
  */
-// write your code here
+func bottleStore(beverage: String, numOfBottles: Int, place: String){
+    print("\(numOfBottles) bottles of \(beverage) on the \(place) wall.")
+}
 
-
-
+bottleStore(beverage: "orange juice", numOfBottles: 25, place: "kitchen")
 
 //: Click [here](https://github.com/learn-co-curriculum/swift-funcMultipleArg-lab/blob/solution/MyPlayground.playground/Pages/solution.xcplaygroundpage/Contents.swift) to see the solution.
