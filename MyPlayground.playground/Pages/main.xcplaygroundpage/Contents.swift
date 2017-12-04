0d177ac7917b82cc9dbf523d9aecef8ee86dbe6d
bbt42 <bruce.tucker.042@gmail.com>
2017-12-04 10:40:51 -0600

    Complete Problems

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -14,7 +14,11 @@
  ### 1. Create a function that will take in a continent and the number of countries in that continent. It should print the following sentence "____ is a continent which contains ___ countries".
  */
 // write your code here
+func continentCountry(continentName: String, numberOfCountries: Int) {
+    print("\(continentName) is a continent which contains \(numberOfCountries) countries.")
+}
 
+continentCountry(continentName: "North America", numberOfCountries: 12)  //call the function
 
 
 
@@ -25,7 +29,7 @@ func greeting(name: String, greeting: String) {
     print("\(greeting), \(name)!")
 }
 
-greeting(name: "Danny", "Hello")
+greeting(name: "Danny", greeting: "Hello")
 
 
 
@@ -33,7 +37,7 @@ greeting(name: "Danny", "Hello")
 /*: question3
  ### 3. This function doesn't work, either. Can you fix the function (_not_ the call) so that it works?
  */
-func daysInMonth(month: String, days: String) {
+func daysInMonth(month: String, days: Int) {
     print("There are \(days) in \(month)")
 }
 
@@ -47,7 +51,10 @@ daysInMonth(month: "November", days: 30)
  */
 // write your code here
 
+func beverageStuff(beverageName: String, numberOfBottles: Int, storagePlace: String) {
+    print("\(numberOfBottles) bottles of \(beverageName) on the \(storagePlace) wall.")
+}
 
-
+beverageStuff(beverageName: "Beer", numberOfBottles: 4, storagePlace: "back")
 
 //: Click [here](https://github.com/learn-co-curriculum/swift-funcMultipleArg-lab/blob/solution/MyPlayground.playground/Pages/solution.xcplaygroundpage/Contents.swift) to see the solution.
