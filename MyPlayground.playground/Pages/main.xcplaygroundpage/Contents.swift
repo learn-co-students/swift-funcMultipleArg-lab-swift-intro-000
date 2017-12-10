a886f758a12b5facfc6297d33d2ef4cdac0e6a37
Joe <joedavini23@yahoo.com>
2017-12-09 20:02:24 -0800

    Function Multiple Arg Lab

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -13,8 +13,10 @@
 /*: question1
  ### 1. Create a function that will take in a continent and the number of countries in that continent. It should print the following sentence "____ is a continent which contains ___ countries".
  */
-// write your code here
-
+func myContinent(continent: String, countries: Int){
+    print("\(continent) is a continent which contains \(countries) countries.")
+}
+myContinent(continent: "North America",countries: 23)
 
 
 
@@ -25,29 +27,31 @@ func greeting(name: String, greeting: String) {
     print("\(greeting), \(name)!")
 }
 
-greeting(name: "Danny", "Hello")
+greeting(name: "Danny", greeting: "Hello")
 
-
+// added greeting to the function call
 
 
 /*: question3
  ### 3. This function doesn't work, either. Can you fix the function (_not_ the call) so that it works?
  */
-func daysInMonth(month: String, days: String) {
+func daysInMonth(month: String, days: Int) {
     print("There are \(days) in \(month)")
 }
 
 daysInMonth(month: "November", days: 30)
 
 
-
+// changed the String to Int in the function
 
 /*: question4
  ### 4. So far, you have created functions that take two arguments. Can you create (and call) one that takes three? Try to create a function that three parameters: a beverage, the number of bottles of that beverage, and a place you can keep those bottles. Print the sentence "____ bottles of ____ on the ____ wall."
  */
-// write your code here
+func myBeverage(beverage: String, bottles: Int, place: String){
+    print("\(bottles) bottles of \(beverage) on the \(place).")
+}
 
-
+myBeverage(beverage: "Beer", bottles: 99, place: "Wall")
 
 
 //: Click [here](https://github.com/learn-co-curriculum/swift-funcMultipleArg-lab/blob/solution/MyPlayground.playground/Pages/solution.xcplaygroundpage/Contents.swift) to see the solution.
