578e04f84468a620a6777e57104e3707892a9a1b
annieflores32 <annieflores32@gmail.com>
2017-12-12 21:29:05 -0600

    multArgs

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -13,9 +13,14 @@
 /*: question1
  ### 1. Create a function that will take in a continent and the number of countries in that continent. It should print the following sentence "____ is a continent which contains ___ countries".
  */
-// write your code here
+func whereInTheWorld (continent: String, numberOfCountries: Int) {
+    print("\(continent) is a continent which contains \(numberOfCountries) countries.")
+}
 
+let continent = "Africa"
+let numberOfCountries = 82
 
+whereInTheWorld(continent: continent, numberOfCountries: numberOfCountries)
 
 
 /*: question2
@@ -25,7 +30,7 @@ func greeting(name: String, greeting: String) {
     print("\(greeting), \(name)!")
 }
 
-greeting(name: "Danny", "Hello")
+greeting (name: "Danny", greeting: "Hello")
 
 
 
@@ -33,7 +38,7 @@ greeting(name: "Danny", "Hello")
 /*: question3
  ### 3. This function doesn't work, either. Can you fix the function (_not_ the call) so that it works?
  */
-func daysInMonth(month: String, days: String) {
+func daysInMonth(month: String, days: Int) {
     print("There are \(days) in \(month)")
 }
 
@@ -45,8 +50,15 @@ daysInMonth(month: "November", days: 30)
 /*: question4
  ### 4. So far, you have created functions that take two arguments. Can you create (and call) one that takes three? Try to create a function that three parameters: a beverage, the number of bottles of that beverage, and a place you can keep those bottles. Print the sentence "____ bottles of ____ on the ____ wall."
  */
-// write your code here
+func allDrinks(beverage: String, numberOfBottles: Int, holder: String) {
+    print(" \(numberOfBottles) bottles of \(beverage) on the \(holder) wall.")
+}
 
+let beverage = "beer"
+let numBottles = 99
+let holder = "back"
+
+allDrinks(beverage: beverage, numberOfBottles: numBottles, holder: holder)
 
 
 
