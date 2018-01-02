6e20f61700692fbe451e0c394a6c61b4dff4657c
thinhthinh <thinhqvu@gmail.com>
2018-01-02 08:32:56 -0500

    Complete multiple arguments func lab

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -14,10 +14,11 @@
  ### 1. Create a function that will take in a continent and the number of countries in that continent. It should print the following sentence "____ is a continent which contains ___ countries".
  */
 // write your code here
+func printCountriesInCont(continent: String, numberOfCountries: Int) {
+    print("\(continent) is a continent which contains \(numberOfCountries) countries")
+}
 
-
-
-
+printCountriesInCont(continent: "Asia", numberOfCountries: 100)
 /*: question2
  ### 2. Can you see why the code below doesn't work? Fix the function call to get rid of the error.
  */
@@ -25,29 +26,24 @@ func greeting(name: String, greeting: String) {
     print("\(greeting), \(name)!")
 }
 
-greeting(name: "Danny", "Hello")
-
-
-
+greeting(name: "Danny", greeting: "Hello")
 
 /*: question3
  ### 3. This function doesn't work, either. Can you fix the function (_not_ the call) so that it works?
  */
-func daysInMonth(month: String, days: String) {
+func daysInMonth(month: String, days: Int) {
     print("There are \(days) in \(month)")
 }
 
 daysInMonth(month: "November", days: 30)
 
-
-
-
 /*: question4
  ### 4. So far, you have created functions that take two arguments. Can you create (and call) one that takes three? Try to create a function that three parameters: a beverage, the number of bottles of that beverage, and a place you can keep those bottles. Print the sentence "____ bottles of ____ on the ____ wall."
  */
 // write your code here
+func bottlesOnWall(beverage: String, numberOfBottles: Int, place: String){
+    print("\(numberOfBottles) bottles of \(beverage) on the \(place) wall")
+}
 
-
-
-
+bottlesOnWall(beverage: "Sprite", numberOfBottles: 5, place: "club")
 //: Click [here](https://github.com/learn-co-curriculum/swift-funcMultipleArg-lab/blob/solution/MyPlayground.playground/Pages/solution.xcplaygroundpage/Contents.swift) to see the solution.
