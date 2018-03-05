e7ff367092072c7432937563a717d6423c9eb13c
Stanley Sakai <sakai.stanley@gmail.com>
2018-03-05 17:42:49 -0500

    Multiple Args Lab

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -13,11 +13,9 @@
 /*: question1
  ### 1. Create a function that will take in a continent and the number of countries in that continent. It should print the following sentence "____ is a continent which contains ___ countries".
  */
-// write your code here
-
-
-
-
+func numCountries(continent: String, countries: Int) {
+    print("\(continent) is a continent which contains \(countries) countries.")
+}
 /*: question2
  ### 2. Can you see why the code below doesn't work? Fix the function call to get rid of the error.
  */
@@ -25,29 +23,30 @@ func greeting(name: String, greeting: String) {
     print("\(greeting), \(name)!")
 }
 
-greeting(name: "Danny", "Hello")
-
+greeting(name: "Danny", greeting: "Hello")
+// Name must be specified for more than one arg.
 
 
 
 /*: question3
  ### 3. This function doesn't work, either. Can you fix the function (_not_ the call) so that it works?
  */
-func daysInMonth(month: String, days: String) {
+func daysInMonth(month: String, days: Int) {
     print("There are \(days) in \(month)")
 }
 
 daysInMonth(month: "November", days: 30)
-
-
-
-
+// Days was originally cast as String but was passed an Int.
 /*: question4
  ### 4. So far, you have created functions that take two arguments. Can you create (and call) one that takes three? Try to create a function that three parameters: a beverage, the number of bottles of that beverage, and a place you can keep those bottles. Print the sentence "____ bottles of ____ on the ____ wall."
  */
-// write your code here
+func bobotw(beverage: String, num: Int, location: String) {
+    print("\(num) bottles of \(beverage) on the \(location)")
+}
 
+let num = 20
+let beverage = "absinthe"
+let location = "suitcase"
 
-
-
+bobotw(beverage: beverage, num: num, location: location)
 //: Click [here](https://github.com/learn-co-curriculum/swift-funcMultipleArg-lab/blob/solution/MyPlayground.playground/Pages/solution.xcplaygroundpage/Contents.swift) to see the solution.
