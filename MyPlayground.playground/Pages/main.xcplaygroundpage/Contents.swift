2d8057c4040a189c569cf88123fd2e5a46d60776
ezsmith <erin.smith.mail@gmail.com>
2018-05-20 15:51:22 -0700

    Complete multiple arguements function lab

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -14,14 +14,16 @@
  ### 1. Create a function that will take in a continent and the number of countries in that continent. It should print the following sentence "____ is a continent which contains ___ countries".
  */
 // write your code here
-
-
+func continentCountry(continent: String, countries: Int){
+    print("\(continent) is a continent which contains \(countries) countries")
+}
+continentCountry(continent: "Australia", countries: 1)
 
 
 /*: question2
  ### 2. Can you see why the code below doesn't work? Fix the function call to get rid of the error.
  */
-func greeting(name: String, greeting: String) {
+func greeting(name: String, _ greeting: String) {
     print("\(greeting), \(name)!")
 }
 
@@ -33,7 +35,7 @@ greeting(name: "Danny", "Hello")
 /*: question3
  ### 3. This function doesn't work, either. Can you fix the function (_not_ the call) so that it works?
  */
-func daysInMonth(month: String, days: String) {
+func daysInMonth(month: String, days: Int) {
     print("There are \(days) in \(month)")
 }
 
