fb7f92d7b75f15fef8d2d11b61838d6a796b7c37
AnastasiosPapalias <anastasios.papalias@gmail.com>
2018-07-22 15:50:51 +0300

    Complete problems

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -13,19 +13,23 @@
 /*: question1
  ### 1. Create a function that will take in a continent and the number of countries in that continent. It should print the following sentence "____ is a continent which contains ___ countries".
  */
-// write your code here
+func completeContinent(_ contName: String, _ contNum: Int) {
+    print("\(contName) is a continent which contains \(contNum) countries")
+}
+let contName = "Africa"
+let contNum = 54
 
-
+completeContinent(contName, contNum)
 
 
 /*: question2
  ### 2. Can you see why the code below doesn't work? Fix the function call to get rid of the error.
  */
-func greeting(name: String, greeting: String) {
+func greetingFunk(name: String, greeting: String) {
     print("\(greeting), \(name)!")
 }
 
-greeting(name: "Danny", "Hello")
+greetingFunk(name: "Danny", greeting: "Hello")
 
 
 
@@ -33,7 +37,7 @@ greeting(name: "Danny", "Hello")
 /*: question3
  ### 3. This function doesn't work, either. Can you fix the function (_not_ the call) so that it works?
  */
-func daysInMonth(month: String, days: String) {
+func daysInMonth(month: String, days: Int) {
     print("There are \(days) in \(month)")
 }
 
@@ -45,9 +49,13 @@ daysInMonth(month: "November", days: 30)
 /*: question4
  ### 4. So far, you have created functions that take two arguments. Can you create (and call) one that takes three? Try to create a function that three parameters: a beverage, the number of bottles of that beverage, and a place you can keep those bottles. Print the sentence "____ bottles of ____ on the ____ wall."
  */
-// write your code here
+func singSong (beverage: String, numBottles: Int, place: String) {
+    print("\(numBottles) of \(beverage) on the \(place)")
+}
 
+let beverage = "Rum"
+let numberBottles = 99
+let placeToBe = "counter top"
 
+singSong(beverage: beverage, numBottles: numberBottles, place: placeToBe)
 
-
-//: Click [here](https://github.com/learn-co-curriculum/swift-funcMultipleArg-lab/blob/solution/MyPlayground.playground/Pages/solution.xcplaygroundpage/Contents.swift) to see the solution.
