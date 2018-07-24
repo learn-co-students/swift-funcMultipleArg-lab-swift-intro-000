db162c5dfc327e155da523bbc53a447205db3ed4
Amar Cheema <amar.s.cheema@gmail.com>
2018-07-24 14:13:25 -0400

    Complete Problems

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -15,8 +15,10 @@
  */
 // write your code here
 
-
-
+func numberOfCountriesInAContinent(this continent:String, has countries: Int) {
+    print ("\(continent) is a continent which contains \(countries) countries.")
+}
+numberOfCountriesInAContinent(this: "North America", has: 2)
 
 /*: question2
  ### 2. Can you see why the code below doesn't work? Fix the function call to get rid of the error.
@@ -25,7 +27,7 @@ func greeting(name: String, greeting: String) {
     print("\(greeting), \(name)!")
 }
 
-greeting(name: "Danny", "Hello")
+greeting(name: "Danny", greeting: "Hello")
 
 
 
@@ -33,7 +35,7 @@ greeting(name: "Danny", "Hello")
 /*: question3
  ### 3. This function doesn't work, either. Can you fix the function (_not_ the call) so that it works?
  */
-func daysInMonth(month: String, days: String) {
+func daysInMonth(month: String, days: Int) {
     print("There are \(days) in \(month)")
 }
 
@@ -47,7 +49,9 @@ daysInMonth(month: "November", days: 30)
  */
 // write your code here
 
+func annoyingCarRideSong(number beer:Int, type alcohol:String, where location:String) {
+    print ("\(beer) bottles of \(alcohol) on the \(location)")
+}
 
-
-
+annoyingCarRideSong(number: 99, type: "beer", where: "wall")
 //: Click [here](https://github.com/learn-co-curriculum/swift-funcMultipleArg-lab/blob/solution/MyPlayground.playground/Pages/solution.xcplaygroundpage/Contents.swift) to see the solution.
